15aef0f3b2d1d19faf253571d96fc4ca9ee0e372
sal-versij <salvoluca1212000@gmail.com>
2018-12-20 10:52:20 +0100

    completed

--- a.js
+++ a.js
@@ -86,16 +86,57 @@ function print(_, t=0) {
 	if(!_)
 		return multi_str('>',t) + " -@- \n";
 	var str = "";
-	str += multi_str('>',t) + _.name+"\n";
+	str += multi_str('>',t) + (_.type=='parameter'?'@':'') + _.name + (_.needed?'':'?') + "\n";
 	for(var i in _.children)
 		str += print(_.children[i], t + 1);
 	return str;
 }
 
+function printAll(_){
+	var str = 'commands:\n';
+	for(i of Object.keys(_))
+		str+=print(_[i])+'\n';
+	return str;
+}
+
+function executeAll(){
+	delete objs,delete commands, delete allCommands;
+	commands = [], allCommands = {};
+	$('.execute-all').click();
+	
+	$.each($('#mw-content-text > div > table.sortable.wikitable.jquery-tablesorter > tbody>tr'),(i,e)=>{
+	$e = $(e);
+	commands.push({
+		'command': $e.find('td:nth-child(1) code').text().match(literal)[1],
+		'description': $e.find('td:nth-child(2)').text().match(/([^\n]*)/i)[1],
+		'edition':{
+            'bedrock': $e.find('td:nth-child(3)').text().match(/yes/i)!=null,
+            'educational': $e.find('td:nth-child(4)').text().match(/yes/i)!=null,
+            'java': $e.find('td:nth-child(5)').text().match(/yes/i)!=null,
+        },
+		'op-level': $e.find('td:nth-child(6)').text().match(/([^\n]*)/i)[1],
+		'multiplayer-only': $e.find('td:nth-child(7)').text().match(/([^\n]*)/i)[1],
+		'modifies/queries':{
+            'block': $e.find('td:nth-child(8)').find('span').length>0,
+            'entities': $e.find('td:nth-child(9)').find('span').length>0,
+            'player': $e.find('td:nth-child(10)').find('span').length>0,
+            'world': $e.find('td:nth-child(11)').find('span').length>0,
+        }
+	});
+});
+	
+	for(i of commands){
+		allCommands[i.command] = objs[i.command];
+		_ = allCommands[i.command];
+		delete i.command;
+		$.extend(true,_, i);
+	}
+}
+
 // clicks all [show] link
 $('.jslink:contains(show)').click();
 
-var literal = /(\w+)/i;
+var literal = /[\\\/]*([\w-_]+)/i;
 var parameter = /<i>(.*?)<\/i>/i;
 var multi_literal = /(\w+(\|\w+)+)/i;
 
@@ -103,9 +144,15 @@ var optional = /\[(.*?)\]/i;
 
 var objs = {};
 function elaborate(){
+	$.each($('code span'),(i,e)=>{
+		$(e).replaceWith($(e).html());
+	});
+	$.each($('code a'),(i,e)=>{
+		$(e).replaceWith($(e).html());
+	});
 	$.each($('code:not(.parsed)'), function (i, e) {
 		// get innerHTML and set .parsed to code to prevent double elaboration
-		var htmls = log($(e).addClass('parsed').html().split(/\s+(?!…)/), "htmls");
+		var htmls = $(e).addClass('parsed').html().split(/\s+(?!…)/);
 
 		var htmls0 = literal.exec(htmls.shift());
 		if (!htmls0)
@@ -122,8 +169,9 @@ function elaborate(){
 		var actual = [obj];
 
 		// {"type": "","name": "","needed": ?,"children": {}}
-		var _;
+		var _, __;
 		for (var html of htmls) {
+			html = html.replace(/\&(l|g)t;/g, '');
 			if ((_ = parameter.exec(html))) {
 				// handle 'html' as parameter
 				var needed = html.match(optional) == null;
@@ -178,6 +226,7 @@ function elaborate(){
 			else
 				objs[obj.name] = obj;
 		})
-		.insertAfter($(e));
+		.insertAfter($(e))
+		.addClass('execute-all');
 	});
 }
