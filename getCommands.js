4c27082e10116bda425d0a574ee361417196060d
sal-versij <salvoluca1212000@gmail.com>
2018-12-21 10:58:45 +0100

    Update and rename a.js to getCommands.js

--- getCommands.js
+++ getCommands.js
@@ -1,79 +1,13 @@
-// Global methods
-function loadJS(url) {
-	$.getScript(url, function () {
-		console.debug(url + ' loaded');
-	});
-}
+// clicks all [show] link
+$('.jslink:contains(show)').click();
 
-function log(..._) {
-	console.log([..._]);
-	return _[0];
-}
+var literal = /[\\\/]*([\w-_]+)/i;
+var parameter = /<i>(.*?)<\/i>/i;
+var multi_literal = /(\w+(\|\w+)+)/i;
 
-function  * range(a, b = undefined, c = undefined, l = undefined) {
-	!b && ([b, a] = [a, 0]);
-	if (l) {
-		while (a < 0)
-			a += l;
-		while (a >= l)
-			a -= l;
-		while (b < 0)
-			b += l;
-		while (b >= l)
-			b -= l;
-	}
-	c = !c && (a > b ? -1 : 1) || c;
-	if ((a > b) ^ (c < 0))
-		return;
-	if (a > b)
-		for (var i = a; i >= b; i += c)
-			yield i;
-	else
-		for (var i = a; i <= b; i += c)
-			yield i;
-}
+var optional = /\[(.*?)\]/i;
 
-// String's methods
-String.prototype.hash = function () {
-	var str = this;
-	var hash = 0;
-	if (str.length <= 0)
-		return hash;
-	for (i = 0; i < str.length; i++) {
-		char = str.charCodeAt(i);
-		hash = ((hash << 5) - hash) + char;
-		hash = hash & hash;
-	}
-	return hash;
-};
-
-String.prototype.f = function (..._) {
-	var s = this;
-	for (var i in _) {
-		s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), _[i]);
-	}
-	return s;
-};
-
-String.prototype.format = function (_) {
-	var s = this;
-	for (var i in _) {
-		s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), _[i]);
-	}
-	return s;
-};
-
-// Array's methods
-Array.prototype.iterate = function  * (a, b, c) {
-	var l = this.length;
-	for (i of range(a, b, c, l)) {
-		yield this[i];
-	}
-};
-
-Array.prototype.get = function (a, b, c) {
-	return [...this.iterate(a, b, c)];
-};
+var objs = {};
 
 function multi_str(s, t){
 	var str = "";
@@ -133,26 +67,10 @@ function executeAll(){
 	}
 }
 
-async function begin(){
-  elaborate();
-  sleep(2000);
-  executeAll();
-  console.log(printAll(allCommands));
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); } 
-
-
-// clicks all [show] link
-$('.jslink:contains(show)').click();
-
-var literal = /[\\\/]*([\w-_]+)/i;
-var parameter = /<i>(.*?)<\/i>/i;
-var multi_literal = /(\w+(\|\w+)+)/i;
-
-var optional = /\[(.*?)\]/i;
-
-var objs = {};
 function elaborate(){
 	$.each($('code span'),(i,e)=>{
 		$(e).replaceWith($(e).html());
@@ -239,4 +157,11 @@ function elaborate(){
 		.insertAfter($(e))
 		.addClass('execute-all');
 	});
+}
+
+async function begin(){
+	elaborate();
+	sleep(2000);
+	executeAll();
+	console.log(printAll(allCommands));
 }
